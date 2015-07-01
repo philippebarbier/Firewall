895f9563b9af900279551bccfd2df858b8d3a8e3
root <root@debian.home>
2015-07-01 09:07:46 +0200

    Update script firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,37 +1,37 @@
 #!/bin/sh 
  
 # Réinitialise les règles
-sudo iptables -t filter -F 
-sudo iptables -t filter -X 
+iptables -t filter -F 
+iptables -t filter -X 
  
 # Bloque tout le trafic
-sudo iptables -t filter -P INPUT DROP 
-sudo iptables -t filter -P FORWARD DROP 
-sudo iptables -t filter -P OUTPUT DROP 
+iptables -t filter -P INPUT DROP 
+iptables -t filter -P FORWARD DROP 
+iptables -t filter -P OUTPUT DROP 
  
 # Connexions localhost
-sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT 
-sudo iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT 
-sudo iptables -t filter -A INPUT -i lo -j ACCEPT 
-sudo iptables -t filter -A OUTPUT -o lo -j ACCEPT 
- 
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT 
+iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT 
+iptables -t filter -A INPUT -i lo -j ACCEPT 
+iptables -t filter -A OUTPUT -o lo -j ACCEPT 
+
 # Ping
-sudo iptables -t filter -A INPUT -p icmp -j ACCEPT 
-sudo iptables -t filter -A OUTPUT -p icmp -j ACCEPT 
+iptables -t filter -A INPUT -p icmp -j ACCEPT 
+iptables -t filter -A OUTPUT -p icmp -j ACCEPT 
  
 # SSH
-sudo iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT 
-sudo iptables -t filter -A OUTPUT -p tcp --dport 22 -j ACCEPT 
+iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT 
+iptables -t filter -A OUTPUT -p tcp --dport 22 -j ACCEPT 
  
 # HTTP
-sudo iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT 
-sudo iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT 
+iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT 
+iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT 
 
 # FTP 
-sudo iptables -t filter -A OUTPUT -p tcp --dport 20:21 -j ACCEPT 
-sudo iptables -t filter -A INPUT -p tcp --dport 20:21 -j ACCEPT 
+iptables -t filter -A OUTPUT -p tcp --dport 20:21 -j ACCEPT 
+iptables -t filter -A INPUT -p tcp --dport 20:21 -j ACCEPT 
 
 # Mail SMTP 
 iptables -t filter -A INPUT -p tcp --dport 25 -j ACCEPT 
 iptables -t filter -A OUTPUT -p tcp --dport 25 -j ACCEPT 
- 
+
